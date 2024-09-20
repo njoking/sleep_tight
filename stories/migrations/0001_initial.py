d9aec120065e743d6761c45bc5e5db5597d332ee
njoking <njokingugi9462@gmail.com>
2024-09-20 22:10:24 +0300

    ttight

--- stories/migrations/0001_initial.py
+++ stories/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Story',
+            name='Stories',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('story_text', models.TextField()),
